ccb6afcd37885a56937aebd4e7a9398bd1c73f91
core.ui <roman.thimian@me.com>
2024-03-10 14:15:03 +0100

    Last answer is automatically copied to the system clipboard

--- pygpt/app.py
+++ pygpt/app.py
@@ -1,26 +1,27 @@
+import pyperclip as pc
+
 from textual import on
-from textual.reactive import reactive
 from textual.app import App, ComposeResult
 from textual.binding import Binding
-from textual.widgets import Input, RichLog, Header, Footer
-
-from pygpt.tui.wd_config import ConfigWindow
-from pygpt.tui.wd_question import QuestionWindow
-from pygpt.tui.wd_message import MessageWindow
-from pygpt.llm.gpt import GptApi
+from textual.reactive import reactive
+from textual.widgets import Footer, Header, Input, RichLog
 
 from pygpt.config import app_config, read_config_file
+from pygpt.llm.gpt import GptApi
+from pygpt.tui.wd_config import ConfigWindow
+from pygpt.tui.wd_message import MessageWindow
+from pygpt.tui.wd_question import QuestionWindow
 
 
 class TexGPT(App):
-    CSS_PATH = 'style.tcss'
+    CSS_PATH = "style.tcss"
 
     BINDINGS = [
-        Binding('ctrl+q', 'request_quit', 'Quit'),
-        Binding('ctrl+s', 'request_config', 'Settings', priority=True, show=True)
+        Binding("ctrl+q", "request_quit", "Quit"),
+        Binding("ctrl+s", "request_config", "Settings", priority=True, show=True),
     ]
 
-    win_titel = reactive('--PyGPT--')
+    win_titel = reactive("--PyGPT--")
 
     def __init__(self, config: dict):
         super().__init__()
@@ -28,12 +29,12 @@ class TexGPT(App):
         self.chat = self.open_ai_chat()
 
     def open_ai_chat(self):
-        if self.app_config.get('openai_api_key') != '':
+        if self.app_config.get("openai_api_key") != "":
             chat = GptApi(
-                self.app_config.get('openai_api_key'),
-                self.app_config.get('model', 'gpt-3.5-turbo-0125'),
-                self.app_config.get('prompt'),
-                self.app_config.get('memory', 4)
+                self.app_config.get("openai_api_key"),
+                self.app_config.get("model", "gpt-3.5-turbo-0125"),
+                self.app_config.get("prompt"),
+                self.app_config.get("memory", 4),
             )
         else:
             chat = None
@@ -42,20 +43,21 @@ class TexGPT(App):
     def compose(self) -> ComposeResult:
         yield Header(show_clock=True)
 
-        yield RichLog(id='view', wrap=True, highlight=True)
-        yield Input(id='input', placeholder='Enter your question')
+        yield RichLog(id="view", wrap=True, highlight=True)
+        yield Input(id="input", placeholder="Enter your question")
 
         yield Footer()
 
     def on_mount(self) -> None:
         self.title = self.win_titel
-        self.set_focus(self.query_one('#input', Input))
+        self.set_focus(self.query_one("#input", Input))
 
     def action_request_quit(self) -> None:
         def check_ok(ok: bool):
             if ok:
                 self.app.exit()
-        self.push_screen(QuestionWindow('Are u sure?'), check_ok)
+
+        self.push_screen(QuestionWindow("Are u sure?"), check_ok)
 
     def action_request_config(self) -> None:
         def check_save(save: bool):
@@ -65,23 +67,26 @@ class TexGPT(App):
 
         self.push_screen(ConfigWindow(self.app_config), check_save)
 
-    @on(Input.Submitted, '#input')
+    @on(Input.Submitted, "#input")
     def get_answer(self, input_field: Input) -> None:
         try:
-            question = {'input': self.chat.prompt.format(question=input_field.value)}
+            question = {"input": self.chat.prompt.format(question=input_field.value)}
 
             result = self.chat.chain.invoke(question)
 
-            rich = self.query_one('#view', RichLog)
-            response = result.get('response')
-            rich.write(f'''{input_field.value}\n\n{response}\n\n''')
-
-            self.query_one('#input').value = ''
+            rich = self.query_one("#view", RichLog)
+            response = result.get("response")
+            rich.write(f"""{input_field.value}\n\n{response}\n\n""")
+            pc.copy(response)
+            self.query_one("#input").value = ""
 
         except Exception as e:
-            self.query_one('#input', Input).value = ''
-            self.push_screen(MessageWindow('No OPENAI_API_KEY was stored or the key is incorrect. '
-                                           'Open the settings and check the OPENAI_API_KEY'))
+            self.query_one("#input", Input).value = ""
+            self.push_screen(
+                MessageWindow(
+                    " No OPENAI_API_KEY was stored or the key is incorrect. " "Open the settings and check the OPENAI_API_KEY"
+                )
+            )
 
 
 def main():
